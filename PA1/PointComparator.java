9a2c087c29eb4413e2e6c41c3f22623194a271d6
belmasab21 <43302038+belmasab21@users.noreply.github.com>
2022-02-06 17:55:52 -0600

    Add files via upload

---
+++ PA1/PointComparator.java
@@ -0,0 +1,9 @@
+
+import java.util.Comparator;
+
+public class PointComparator implements Comparator<Point> {
+	@Override
+	public int compare(Point arg1, Point arg2) {
+		return arg1.x <= arg2.x ? -1 : 1;
+	}
+}
