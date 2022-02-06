18333b032838844bb71c66297629e474a8fab58f
belmasab21 <43302038+belmasab21@users.noreply.github.com>
2022-02-06 17:53:16 -0600

    Delete IntegerComparator.java

--- IntegerComparator.java
+++
@@ -1,9 +0,0 @@
-
-import java.util.Comparator;
-
-public class IntegerComparator implements Comparator<Integer> {
-	@Override
-	public int compare(Integer arg1, Integer arg2) {
-		return arg1 -  arg2;
-	}
-}
